3c2ca2a9e5f1b80fc733626b8a8608603768a11d
Stone.F.Yang (netw devp.twtp01.Newegg) 61589 <sy43@buyabs.corp>
2017-11-02 15:46:46 +0800

    first commit

---
+++ SignalR/ChatHub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR
+{
+    public class ChatHub : Hub
+    {
+        public void Hello(string name)
+        {
+            //這邊會傳入name參數
+            //呼叫所有連線狀態中頁面上的 javascript function => hello
+            //透過server端呼叫client的javascript function
+            var userId = Context.ConnectionId;
+            string message = "歡迎使用者 " + name + " 加入聊天室";
+            Clients.All.hello(message);
+        }
+
+        public void SendMessage(string name, string message)
+        {
+            //這邊會傳入name和message參數
+            //並且會呼叫所有連線狀態中頁面上的 javascript function => sendAllMessage
+            //透過server端呼叫client的javascript function
+            var userId = Context.ConnectionId;
+            message = name + "說：" + message;
+
+            Clients.All.sendAllMessge(message);
+            //Clients.Client(Context.ConnectionId).sendAllMessge(message);
+        }
+    }
+}
---
+++ SignalR/SingleHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalR
+{
+    public class SingleHub : Hub
+    {
+        public void SendMessageToServer(string name, string message)
+        {
+            var userId = Context.ConnectionId;
+            message = name + "獨自說：" + message;
+
+            Clients.Client(Context.ConnectionId).sendMessgeToClient(message);
+        }
+    }
+}
